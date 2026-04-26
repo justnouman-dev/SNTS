fe7bdd6f8e7c3d5d708fce9788a4c89b73e6d9fa
nouman <262111777+justnouman-dev@users.noreply.github.com>
2026-04-26 21:55:08 +0530

    Delete app directory

--- app/models/__init__.py
+++
@@ -1,6 +0,0 @@
-"""
-Models Package Initializer
-"""
-from .database import db, User, HealthSuggestion, FoodLog, HydrationLog, Reward
-
-__all__ = ['db', 'User', 'HealthSuggestion', 'FoodLog', 'HydrationLog', 'Reward']
